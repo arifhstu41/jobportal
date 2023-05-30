59c385f19ee7c7a27b9fd0c176c49d564d82180b
Khairat Hossin <khairat564@gmail.com>
2023-05-31 02:32:40 +0600

    manual payment verification cron done

--- app/Http/Controllers/Admin/PaymentVerificationController.php
+++ app/Http/Controllers/Admin/PaymentVerificationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Earning;
-use App\Models\ManualPayment;
 use App\Models\PaymentModel;
 use App\Models\PaymentVerification;
 use App\Models\User;
@@ -14,6 +13,18 @@ use ShurjopayPlugin\Shurjopay;
 use ShurjopayPlugin\ShurjopayEnvReader;
 
 class PaymentVerificationController extends Controller {
+
+    public $surjopay_credit_amount;
+
+    /**
+     * constructor
+     */
+    function __construct() {
+
+        $this->surjopay_credit_amount = env('SURJOPAY_CREDIT_AMOUNT');
+
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -48,44 +59,43 @@ class PaymentVerificationController extends Controller {
             $transaction_id = $request->transaction_id;
             // DB::beginTransaction();
             // try {
-                $payment_verification = PaymentVerification::where('user_id', $user_id)->where('status', 0)->first();
+            $payment_verification = PaymentVerification::where('user_id', $user_id)->where('status', 0)->first();
 
-                if (!$payment_verification) {
-                    $payment_verification                 = new PaymentVerification();
-                    $payment_verification->user_id        = $user_id;
-                    $payment_verification->transaction_id = $request->transaction_id;
-                    $payment_verification->created_at     = now();
-                    $payment_verification->status         = 0;
-                    $payment_verification->save();
-                }
-                $verify = $this->verifyPayment($user_id);
-                if ($verify) {
-                    $payment_verification->status = 1;
-                    $payment_verification->save();
-                    flashSuccess('Payment Verification successful');
-                }
-                else{
-                    flashError('Cannot verify payment, No successful payment found');
-                }
-                // DB::commit();
-                
-                return redirect()->route('payment.verification');
+            if (!$payment_verification) {
+                $payment_verification                 = new PaymentVerification();
+                $payment_verification->user_id        = $user_id;
+                $payment_verification->transaction_id = $request->transaction_id;
+                $payment_verification->created_at     = now();
+                $payment_verification->status         = 0;
+                $payment_verification->save();
+            }
+            $verify = $this->verifyPayment($user_id);
+            if ($verify) {
+                $payment_verification->status = 1;
+                $payment_verification->save();
+                flashSuccess('Payment Verification successful');
+            } else {
+                flashError('Cannot verify payment, No successful payment found');
+            }
+            // DB::commit();
+
+            return redirect()->route('payment.verification');
             // } catch (\Throwable $th) {
-                dd($th->getMessage());
-                // DB::rollBack();
-                flashError('something went wrong');
-                return back()->withInput();
+            dd($th->getMessage());
+            // DB::rollBack();
+            flashError('something went wrong');
+            return back()->withInput();
             // }
-        }
-        else{
+        } else {
             flashError('User not found');
             return back()->withInput();
         }
     }
 
     public function verifyPayment($user_id) {
-        $user= User::find($user_id);
+        $user    = User::find($user_id);
         $payment = PaymentModel::where('user_id', $user_id)->orderBy('id', 'DESC')->first();
+
         if ($payment) {
             $env                = new ShurjopayEnvReader(base_path() . '/.env');
             $conf               = $env->getConfig();
@@ -95,16 +105,18 @@ class PaymentVerificationController extends Controller {
             $data               = (array) $data[0];
             $data['user_id']    = $user->id;
 
-            $this->updatePayment($data);
-            $this->updateEarning($data);
-            $user->candidate->balance = $data['amount'];
-            $user->candidate->save();
+            // if payment is successful then update candidate balance
+            if ($data['sp_code'] == 1000) {
+                $this->updatePayment($data);
+                $this->updateEarning($data);
+                $user->candidate->balance = $data['amount'];
+                $user->candidate->save();
+            } else {
+                return false;
+            }
             return true;
         }
         return false;
-        flashError('No payment record available');
-        return redirect()->route('payment.verification')->withInput();
-
     }
 
     // create payment
@@ -248,5 +260,93 @@ class PaymentVerificationController extends Controller {
      */
     public function destroy($id) {
         //
+    }
+
+    // cronjob function
+    public function verifyPaymentCron() {
+        $users = [];
+        if (request()->phone) {
+            $users[] = User::where('phone', request()->phone)->pluck('id')->first();
+        } else {
+            $users = PaymentVerification::where('status', 0)->pluck('user_id')->all();
+        }
+        
+        DB::beginTransaction();
+        try {
+        foreach ($users as $user_id) {
+            /**
+             * if payment verification is successful then its ok to continue do nothing
+             * else make payment manually
+             */
+            $verify = $this->verifyPayment($user_id);
+            if (!$verify) {
+                $user = User::find($user_id);
+                if ($user) {
+                    $data = $this->makePaymentData($user);
+                    $this->createPayment($user, $data);
+                    $this->createEarning($user, $data);
+
+                    $user->candidate->balance = $data['amount'];
+                    $user->candidate->save();
+
+                    PaymentVerification::where('user_id', $user->id)->update(['status' => 1]);
+                }
+            }
+        }
+        DB::commit();
+        if(request()->phone){
+            flashSuccess('Payment verified successfully');
+            return redirect()->back();
+        }
+        else{
+            return true;
+        }
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            if(request()->phone){
+                flashError('something went wrong');
+                return redirect()->back();
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    // make payment data
+    public function makePaymentData(User $user) {
+        $data['id']                 = time();
+        $data['order_id']           = "WFB" . time();
+        $data['currency']           = "BDT";
+        $data['amount']             = $this->surjopay_credit_amount;
+        $data['payable_amount']     = $this->surjopay_credit_amount;
+        $data['discount_amount']    = 0;
+        $data['disc_percent']       = 0;
+        $data['recived_amount']     = $this->surjopay_credit_amount;
+        $data['usd_amt']            = 0;
+        $data['usd_rate']           = 0;
+        $data['card_holder_name']   = $user->name;
+        $data['card_number']        = "";
+        $data['phone_no']           = $user->phone;
+        $data['bank_trx_id']        = "";
+        $data['invoice_no']         = "";
+        $data['bank_status']        = "Success";
+        $data['customer_order_id']  = "";
+        $data['sp_code']            = 1000;
+        $data['sp_message']         = "Success";
+        $data['name']               = $user->name;
+        $data['email']              = $user->email;
+        $data['address']            = "Rangamati";
+        $data['city']               = "Rangamati";
+        $data['value1']             = "value1";
+        $data['value2']             = "value2";
+        $data['value3']             = "value3";
+        $data['value4']             = "value4";
+        $data['transaction_status'] = "Success";
+        $data['method']             = "manual";
+        $data['date_time']          = date("YYYY-mm-DD H:i:s");
+
+        return $data;
     }
 }
